042205c5b26c8e1eac608a2c335780c5fabe31d1
tidypr <tidypr@icloud.com>
2025-12-30 14:48:50 +0900

    chore: theme provider 설정

--- src/shared/providers/index.tsx
+++ src/shared/providers/index.tsx
@@ -1,6 +1,8 @@
+import ThemeProvider from './ThemeProvider';
+
 /**
  * 루트 레이아웃을 감싸는 프로바이더 컴포넌트
- * 추후 필요한 프로바이더들을 추가할 수 있는 기본 구조 제공
+ * 테마 관리를 포함한 모든 프로바이더들을 통합하여 제공
  *
  * @param {Object} props - 컴포넌트 props
  * @param {React.ReactNode} props.children - 자식 컴포넌트
@@ -11,5 +13,5 @@ export default function Providers({
 }: {
   children: React.ReactNode;
 }): React.ReactElement {
-  return <>{children}</>;
+  return <ThemeProvider>{children}</ThemeProvider>;
 }
